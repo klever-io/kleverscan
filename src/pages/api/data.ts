02f5f1a46c29d1d743defbdefaef0df0c2e1f51c
Samuel Façanha <samufacanha@gmail.com>
2022-01-07 19:33:21 -0300

    added redis status validation (#157)

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -8,6 +8,7 @@ import api, { Service } from '@/services/api';
 const redis = new Redis({
   host: process.env.DEFAULT_REDIS_HOST || '10.196.89.28',
   port: Number(process.env.DEFAULT_REDIS_PORT) || 6379,
+  connectTimeout: Number(process.env.DEFAULT_REDIS_CONNECT_TIMEOUT) || 5000,
 });
 
 interface IProps {
@@ -25,19 +26,29 @@ const middleware = async (
   res: NextApiResponse,
 ): Promise<any> => {
   const { route, service, refreshTime }: IProps = req.body;
+  if (redis.status === 'ready') {
+    let cache = await redis.get(`cache::${route}`);
 
-  let cache = await redis.get(`cache::${route}`);
+    if (cache) {
+      cache = JSON.parse(cache);
 
-  if (cache) {
-    cache = JSON.parse(cache);
+      res.status(200).json(cache);
+    } else {
+      const response: DataResponse = await api.get({ route, service });
 
-    res.status(200).json(cache);
+      if (!response.error) {
+        redis.set(
+          `cache::${route}`,
+          JSON.stringify(response),
+          'EX',
+          refreshTime,
+        );
+      }
+
+      res.status(200).json(response);
+    }
   } else {
     const response: DataResponse = await api.get({ route, service });
-
-    if (!response.error) {
-      redis.set(`cache::${route}`, JSON.stringify(response), 'EX', refreshTime);
-    }
 
     res.status(200).json(response);
   }

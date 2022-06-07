5ee67c723095220d7fb1879187256815e28e53f4
Lucas Portella <63923113+lucasportella@users.noreply.github.com>
2022-06-07 18:55:23 -0300

    KC-621-fix-tooltip-component-test-after-refactor (#297)
    * test(Tooltip-component): correct tooltip component unit test after refactor
    
    * test(Tooltip-component): test component disappearing after unhover

--- src/components/Tooltip/Tooltip.spec.tsx
+++ src/components/Tooltip/Tooltip.spec.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
-import { screen } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
 
 import { renderWithTheme } from '../../test/utils/';
 
-import  Tooltip  from './index';
+import Tooltip from './index';
 
 describe('Tooltip', () => {
-  test('', () => {
-    const message = "Always check the address. Names can be the same across multiple addresses."
-    renderWithTheme(<Tooltip msg={message} />).container;
-    const tooltipMsg = screen.getByText(message);
-    expect(tooltipMsg).toBeInTheDocument()
+  const message =
+    'Always check the address. Names can be the same across multiple addresses.';
+  test('Tooltip rendering before and after hover', async () => {
+    const container = renderWithTheme(
+      <Tooltip data-testid="tooltipId" msg={message} />,
+    ).container;
+    const tooltipController =
+      container.firstElementChild?.firstElementChild?.firstElementChild;
+    let tooltipMsg = screen.queryByText(message);
+    expect(tooltipMsg).not.toBeInTheDocument();
+    waitFor(async () => {
+      await userEvent.hover(tooltipController);
+    });
+    tooltipMsg = screen.getByText(message);
+    expect(tooltipMsg).toBeInTheDocument();
     expect(tooltipMsg?.previousElementSibling?.nodeName).toBe('svg');
-  })
-})
+
+    waitFor(async () => {
+      await userEvent.unhover(tooltipController);
+    });
+    expect(tooltipMsg).not.toBeInTheDocument();
+  });
+});

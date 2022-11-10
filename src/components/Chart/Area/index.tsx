075858f66d9a98e1d25035459604db5224dd18db
Doug Fernando <72462482+Doug-Fernando@users.noreply.github.com>
2022-11-09 21:04:30 -0300

    Fix resizeOberver error (#722)

--- src/components/Chart/Area/index.tsx
+++ src/components/Chart/Area/index.tsx
@@ -13,35 +13,37 @@ interface IChart {
 
 const Chart: React.FC<IChart> = ({ data, theme, bg = 'regular' }) => {
   return (
-    <ResponsiveContainer width="100%" height="100%" debounce={300}>
-      <AreaChart data={data}>
-        <defs>
-          <linearGradient id="areaBackground" x1="0" y1="0" x2="0" y2="1">
-            <stop
-              offset="10%"
-              stopColor={
-                bg === 'regular'
-                  ? transparentize(0.5, theme.chart.lightBg)
-                  : theme.chart.darkBg
-              }
-              stopOpacity={0.9}
-            />
-            <stop
-              offset="90%"
-              stopColor={bg === 'regular' ? theme.violet : theme.chart.darkBg}
-              stopOpacity={0}
-            />
-          </linearGradient>
-        </defs>
-        <Area
-          type="monotone"
-          dataKey="value"
-          strokeWidth={0}
-          fillOpacity={1}
-          fill="url(#areaBackground)"
-        />
-      </AreaChart>
-    </ResponsiveContainer>
+    <div style={{ width: '100%', height: '100%' }}>
+      <ResponsiveContainer width="100%" height="100%" debounce={500}>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id="areaBackground" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="10%"
+                stopColor={
+                  bg === 'regular'
+                    ? transparentize(0.5, theme.chart.lightBg)
+                    : theme.chart.darkBg
+                }
+                stopOpacity={0.9}
+              />
+              <stop
+                offset="90%"
+                stopColor={bg === 'regular' ? theme.violet : theme.chart.darkBg}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <Area
+            type="monotone"
+            dataKey="value"
+            strokeWidth={0}
+            fillOpacity={1}
+            fill="url(#areaBackground)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 

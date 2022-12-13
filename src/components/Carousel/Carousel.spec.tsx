53da20a8af8b7faaaf4b0bce75648a49d7160228
Lucas Portella <lucasportellaagu@gmail.com>
2022-12-13 21:04:46 +0000

    test(Carousel-component): implement test (#793)
    * test(Carousel-component): implement test
    
    * test(Carousel-component): finish test

---
+++ src/components/Carousel/Carousel.spec.tsx
@@ -0,0 +1,28 @@
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import Carousel from '.';
+import { renderWithTheme } from '../../test/utils';
+
+describe('test Carousel component', () => {
+  it('should render the component and clicking the buttons should trigger useRef functions', () => {
+    const mockedRefs = jest.fn((): any => {
+      current: null;
+    });
+    jest.spyOn(React, 'useRef').mockReturnValue(mockedRefs());
+
+    const { container } = renderWithTheme(<Carousel />);
+    expect(mockedRefs).toBeCalled();
+
+    const domSnapshot1 = window.document.documentElement.outerHTML;
+
+    const leftArrow = container.firstElementChild?.firstElementChild;
+    expect(leftArrow?.nodeName).toBe('svg');
+    const rightArrow = leftArrow?.nextElementSibling?.nextElementSibling;
+    expect(rightArrow?.nodeName).toBe('svg');
+    fireEvent.click(leftArrow as Element);
+    fireEvent.click(rightArrow as Element);
+    // no changes in DOM/UI
+    const domSnapshot2 = window.document.documentElement.outerHTML;
+    expect(domSnapshot1).toEqual(domSnapshot2);
+  });
+});

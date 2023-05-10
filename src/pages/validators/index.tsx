5b4021af2885542d1564e2692f08bfa47e0fc332
Doug Fernando <72462482+Doug-Fernando@users.noreply.github.com>
2023-05-10 15:51:01 -0300

    fix: fix validators order (#1084)

--- src/pages/validators/index.tsx
+++ src/pages/validators/index.tsx
@@ -60,7 +60,7 @@ const Validators: React.FC = () => {
     const localQuery = { ...router.query, page, limit };
     const validators = await api.get({
       route: 'validator/list',
-      query: localQuery,
+      query: { sort: 'elected', ...localQuery },
     });
 
     if (!validators.error) {

5ce7dcc9681e63be0d2059c3c5f4855f43e5fdb3
Samuel Façanha <samufacanha@gmail.com>
2023-10-05 15:04:48 -0300

    chore: prompt connection on wizard open (#1273)

--- src/components/Wizard/index.tsx
+++ src/components/Wizard/index.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useExtension } from '@/contexts/extension';
+import { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import CreateAssetWizard from './createAsset';
 import { CloseModal, WizardContainer, WizardModal } from './createAsset/styles';
 
 const Wizard: React.FC<any> = ({ isOpen, closeModal }) => {
   const [txHash, setTxHash] = useState('');
+  const { extensionInstalled, connectExtension } = useExtension();
 
   const createWizProps = {
     isOpen,
     txHash,
     setTxHash,
   };
+
+  useEffect(() => {
+    if (extensionInstalled) {
+      connectExtension();
+    }
+  }, [extensionInstalled]);
 
   return (
     <WizardModal openModal={isOpen}>

7111ea3bff7b2f6ea40837cd9277e2b4dc21a5c2
Toitio <79183001+JoseToitio@users.noreply.github.com>
2023-11-17 11:09:15 -0300

    chore(verify): translate(pt-br) page (#56)

--- src/pages/verify-signature/index.tsx
+++ src/pages/verify-signature/index.tsx
@@ -9,15 +9,20 @@ import {
   Header,
 } from '@/styles/common';
 import { Content } from '@/views/verify-signature/detail';
+import { GetStaticProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useState } from 'react';
+import nextI18nextConfig from '../../../next-i18next.config';
 
 const VerifySignature: React.FC = () => {
-  const cardHeaders = ['Verify Signature'];
+  const { t } = useTranslation('verify');
+  const cardHeaders = [`${t('Verify')} ${t('Signature')}`];
   const [selectedCard, setSelectedCard] = useState<string>(cardHeaders[0]);
 
   const SelectedComponent: React.FC = () => {
     switch (selectedCard) {
-      case 'Verify Signature':
+      case `${t('Verify')} ${t('Signature')}`:
         return <FormVerifySignature />;
       default:
         return <div />;
@@ -27,7 +32,7 @@ const VerifySignature: React.FC = () => {
   return (
     <Container>
       <Header>
-        <Title title="Verify" Icon={Icon} />
+        <Title title={t('Verify')} Icon={Icon} />
       </Header>
       <Content>
         <CardHeader>
@@ -47,6 +52,16 @@ const VerifySignature: React.FC = () => {
       </Content>
     </Container>
   );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => {
+  const props = await serverSideTranslations(
+    locale,
+    ['verify'],
+    nextI18nextConfig,
+  );
+
+  return { props };
 };
 
 export default VerifySignature;

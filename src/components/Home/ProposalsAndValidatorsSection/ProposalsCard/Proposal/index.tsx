dd9aa16b63faf3fb80db365180aa3bb037d69761
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-07-19 10:36:32 -0300

    fix 🐞 proposal wrong href

--- src/components/Home/ProposalsAndValidatorsSection/ProposalsCard/Proposal/index.tsx
+++ src/components/Home/ProposalsAndValidatorsSection/ProposalsCard/Proposal/index.tsx
@@ -1,43 +1,30 @@
-import { PropsWithChildren } from 'react';
-import { PurpleArrowRight } from '@/assets/icons';
-import { Status } from '@/components/Table/styles';
-import { getProposalStatusColorAndText } from '@/components/Tabs/Proposals';
-import { IProposal } from '@/types/proposals';
-import {
-  ArrowIconContainer,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  InnerCardContainer,
-} from '../../style';
+import { PropsWithChildren } from 'react'
+import { PurpleArrowRight } from '@/assets/icons'
+import { Status } from '@/components/Table/styles'
+import { getProposalStatusColorAndText } from '@/components/Tabs/Proposals'
+import { IProposal } from '@/types/proposals'
+import { ArrowIconContainer, CardDescription, CardHeader, CardTitle, InnerCardContainer } from '../../style'
 
 interface ProposalProps {
-  proposal: IProposal;
+	proposal: IProposal
 }
 
-export const Proposal: React.FC<PropsWithChildren<ProposalProps>> = ({
-  proposal,
-}) => {
-  const { description, proposalId, proposalStatus } = proposal;
+export const Proposal: React.FC<PropsWithChildren<ProposalProps>> = ({ proposal }) => {
+	const { description, proposalId, proposalStatus } = proposal
 
-  const proposalStatusColorAndText =
-    getProposalStatusColorAndText(proposalStatus);
-  return (
-    <InnerCardContainer>
-      {proposalId && (
-        <CardHeader>
-          <CardTitle href={`/proposals/${proposalId}`}>
-            Proposal #{proposalId}
-          </CardTitle>
-          <ArrowIconContainer href={`/proposals/${proposalId}`}>
-            <PurpleArrowRight />
-          </ArrowIconContainer>
-          <Status status={proposalStatusColorAndText.color}>
-            {proposalStatusColorAndText.text}
-          </Status>
-        </CardHeader>
-      )}
-      <CardDescription>{description}</CardDescription>
-    </InnerCardContainer>
-  );
-};
+	const proposalStatusColorAndText = getProposalStatusColorAndText(proposalStatus)
+	return (
+		<InnerCardContainer>
+			{proposalId && (
+				<CardHeader>
+					<CardTitle href={`/proposal/${proposalId}`}>Proposal #{proposalId}</CardTitle>
+					<ArrowIconContainer href={`/proposal/${proposalId}`}>
+						<PurpleArrowRight />
+					</ArrowIconContainer>
+					<Status status={proposalStatusColorAndText.color}>{proposalStatusColorAndText.text}</Status>
+				</CardHeader>
+			)}
+			<CardDescription>{description}</CardDescription>
+		</InnerCardContainer>
+	)
+}

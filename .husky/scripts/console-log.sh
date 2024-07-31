dabe493ade0ac3d9adb6a3872fcf54600daec276
saviojks <savio@klever.io>
2024-07-31 14:06:14 -0300

    fix: 🐞 sh in vscode

--- .husky/scripts/console-log.sh
+++ .husky/scripts/console-log.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-# Redirect output to stderr.
-exec 1>&2
 
+# Define a expressão regular para console.log
 consoleregexp='console.log'
 
 # Verifica se há ocorrências de console.log no diff em cache, excluindo o próprio script
-if git diff --cached | grep -E '^\+\+\+ b/.*' | grep -vE '^\+\+\+ b/.husky/scripts/console-log.sh' | xargs git diff --cached | grep -q $consoleregexp; then
-  # Mostra as linhas com console.log, excluindo o próprio script
-  git diff --cached | grep -E '^\+\+\+ b/.*' | grep -vE '^\+\+\+ b/.husky/scripts/console-log.sh' | xargs git diff --cached | grep -ne $consoleregexp
-
-  # Pergunta ao usuário se ele deseja continuar com o commit
-  read -p "There are some occurrences of console.log in your modifications. Are you sure you want to continue? (y/n) " yn
-  if echo "$yn" | grep -iq "^y$"; then
-    exit 0
-  else
+if git diff --cached | grep -q $consoleregexp; then
+  # Verifica se as ocorrências estão fora do próprio script
+  if git diff --cached | grep -q $consoleregexp | grep -vE '^\+\+\+ b/.husky/scripts/console-log.sh'; then
+    # Mostra uma mensagem de erro e impede o commit
+    echo "Error: There are some occurrences of console.log in your modifications (excluding .husky/scripts/console-log.sh). Please remove them before committing."
     exit 1
   fi
 fi
+
+# Permite o commit
+exit 0

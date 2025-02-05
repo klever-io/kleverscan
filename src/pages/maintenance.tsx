43c99ede51d8909267a4a0c207a59be3dc92c949
lucas <lucasportellaagu@gmail.com>
2025-02-05 19:58:41 -0300

    feat: image desktop and mobile

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,15 +1,22 @@
 import { LearnMore, Main, P1, P2, Title1, Title3 } from './maintenanceStyles';
-import Image from 'next/image';
 
 const Maintenance = () => {
   return (
     <Main>
-      <Image
-        src="/maintenance-mobile.png"
-        alt="maintenance"
-        width={350}
-        height={350}
-      />
+      <picture>
+        <source
+          srcSet="/maintenance-desktop.png"
+          media="(max-width: 768px)"
+          width={350}
+          height={350}
+        />
+        <img
+          src="/maintenance-mobile.png"
+          alt="Responsive"
+          width={500}
+          height={500}
+        />
+      </picture>
       <Title1>Klever Blockchain Under Scheduled Maintenance</Title1>
       <P1>
         We are upgrading the Klever Blockchain Testnet to integrate the latest
@@ -17,7 +24,7 @@ const Maintenance = () => {
         scalability.
       </P1>
       <Title3>February 7th, 5 PM UTC</Title3>
-      <P2>A new era for smart contracts is comming!</P2>
+      <P2>A new era for smart contracts is coming!</P2>
       <LearnMore>
         <a
           href="https://forum.klever.org/t/klever-blockchain-testnet-scheduled-maintenance-a-major-leap-for-kvm/3449"

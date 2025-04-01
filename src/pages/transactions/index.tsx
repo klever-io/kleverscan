40041555505804d3d9a09b5b2e92d2aee8ee484a
Moisés Abraão Sousa de Oliveira <moises@klever.io>
2025-04-01 15:26:51 -0300

    fix: wrong precision of home transactions and asset transactions

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -86,7 +86,7 @@ export const mobileAddressSectionElement = (toAddress: string): JSX.Element => {
   );
 };
 
-const getAssetsAndCurrenciesList = (
+export const getAssetsAndCurrenciesList = (
   contract: IContract,
   transaction: ITransaction,
 ): string[] => {
@@ -119,7 +119,7 @@ const getAssetsAndCurrenciesList = (
   return assets;
 };
 
-const getTransactionPrecision = (
+export const getTransactionPrecision = (
   contract: IContract,
   transaction: ITransaction,
   assetPrecisions: { [key: string]: number },

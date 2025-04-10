3c93622a76757bf831c71e038f4cdb3f389f365f
Moisés Abraão Sousa de Oliveira <moises@klever.io>
2025-04-10 10:11:30 -0300

    fix: add split royalties on asset page

--- src/components/TransactionForms/CustomForms/utils/index.ts
+++ src/components/TransactionForms/CustomForms/utils/index.ts
@@ -32,7 +32,7 @@ export const parseSplitRoyalties = (data: any): void => {
 
   splitRoyaltiesReference.forEach((item: any) => {
     const address = item?.address;
-    delete item.address;
+    delete item?.address;
     splitRoyalties[address] = item;
   });
   data.royalties.splitRoyalties = splitRoyalties;

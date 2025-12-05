2bc69642bdd84b8415b09c64fb33fb119041c1ce
Fernando Sobreira <fbsobreira@gmail.com>
2025-12-05 10:53:51 -0500

    fix: improve console.log detection script for better quoting and user prompt

--- .husky/scripts/console-log.sh
+++ .husky/scripts/console-log.sh
@@ -4,15 +4,14 @@ exec 1>&2
 
 consoleregexp='console.log'
 # CHECK
-if test $(git diff --cached | grep $consoleregexp | wc -l) != 0
+if test $(git diff --cached | grep "$consoleregexp" | wc -l) != 0
 then
-  git diff --cached | grep -ne $consoleregexp
+  git diff --cached | grep -ne "$consoleregexp"
 
   # Check if /dev/tty is available for interactive prompt
-  if [ -e /dev/tty ]; then
-    exec < /dev/tty
-    read -p "There are some occurrences of console.log at your modification. Are you sure want to continue? (y/n)" yn
-    echo $yn | grep ^[Yy]$
+  if exec < /dev/tty 2>/dev/null; then
+    printf "There are some occurrences of console.log at your modification. Are you sure want to continue? (y/n) " && read yn
+    printf '%s' "$yn" | grep -q '^[Yy]$'
     if [ $? -eq 0 ]
     then
       exit 0; #THE USER WANTS TO CONTINUE

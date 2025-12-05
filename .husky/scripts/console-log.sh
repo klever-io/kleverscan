81c4c04da81f5cf77eb2ac34c5cf843a20b950f9
Fernando Sobreira <fbsobreira@gmail.com>
2025-12-05 09:53:14 -0500

    fix: update husky script

--- .husky/scripts/console-log.sh
+++ .husky/scripts/console-log.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 # Redirect output to stderr.
 exec 1>&2
-# enable user input
-exec < /dev/tty
 
 consoleregexp='console.log'
 # CHECK
 if test $(git diff --cached | grep $consoleregexp | wc -l) != 0
-then 
-  exec git diff --cached | grep -ne $consoleregexp
-  read -p "There are some occurrences of console.log at your modification. Are you sure want to continue? (y/n)" yn
-  echo $yn | grep ^[Yy]$
-  if [ $? -eq 0 ] 
-  then
-    exit 0; #THE USER WANTS TO CONTINUE
+then
+  git diff --cached | grep -ne $consoleregexp
+
+  # Check if /dev/tty is available for interactive prompt
+  if [ -e /dev/tty ]; then
+    exec < /dev/tty
+    read -p "There are some occurrences of console.log at your modification. Are you sure want to continue? (y/n)" yn
+    echo $yn | grep ^[Yy]$
+    if [ $? -eq 0 ]
+    then
+      exit 0; #THE USER WANTS TO CONTINUE
+    else
+      exit 1; # THE USER DONT WANT TO CONTINUE SO ROLLBACK
+    fi
   else
-    exit 1; # THE USER DONT WANT TO CONTINUE SO ROLLBACK
+    echo "Warning: console.log found but cannot prompt (no tty). Allowing commit."
+    exit 0;
   fi
 fi
